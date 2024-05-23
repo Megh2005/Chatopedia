eaadb204fa1e840534c7dfd10306e1544f8af9e0
Megh Deb <20169629-pheonixflaunt@users.noreply.gitlab.com>
2024-05-23 22:32:40 +0530

    code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -41,3 +41,49 @@ export const register = async (req, res) => {
     });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!user || !isPassword) {
+      res.status(500).json({
+        success: false,
+        error: "Invalid Username or Password",
+      });
+    }
+    createToken(user._id, res);
+    return res.status(201).json({
+      success: true,
+      message: "User Logged In Successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        userid: user._id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Something Went Wrong",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt");
+    res.status(201).json({
+      success: true,
+      message: "User Logged Out Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Something Went Wrong",
+    });
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { register } from "../controllers/user.controller.js";
+import { login, logout, register } from "../controllers/user.controller.js";
 const router = express.Router();
 router.post("/register", register);
+router.post("/login", login);
+router.post("/logout",logout)
 
 export default router;
